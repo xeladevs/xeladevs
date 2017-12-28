d6942ed93daccb2fbbd1fc58421158af91458139
Andre <xeladevs@gmail.com>
2017-12-27 22:21:52 -0600

    Primer commit de pagina oficial en git

---
+++ js/ocultar.js
@@ -0,0 +1,10 @@
+// jQuery
+
+$(document).ready(function(){
+  $('.nav-item').on('click',function(){
+      // $('#navbarTogglerDemo01').hide('slow');
+       $('.navbar-collapse').collapse('hide');
+    });
+
+
+});
